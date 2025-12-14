d29dde63e6b01c1279b3020fb6278b3d5369b5f9
Skorp <12578071+Skorpo@users.noreply.github.com>
2025-12-14 15:08:27 -0500

    Installed react native paper

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Phantasm</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F3F1E7',
     alignItems: 'center',
     justifyContent: 'center',
   },
